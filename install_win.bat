3fb6f73ed1a1c5178a8bab580cbf27ea2c59ed48
JeenQ Lee <21200590@handong.edu>
2017-11-06 11:23:19 +0900

    Update install_win.bat

--- install_win.bat
+++ install_win.bat
@@ -1,10 +1,10 @@
 @echo off
 REM git clone
-call git clone https://github.com/JeenQ/Video_Archive_Web_Service.git
+REM call git clone https://github.com/JeenQ/Video_Archive_Web_Service.git
 
 REM install bitnami
-powershell "(new-object Net.WebClient).DownloadFile('https://bitnami.com/redirect/to/163844/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe', 'Video_Archive_Web_Service/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe')"
-Video_Archive_Web_Service\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe
+powershell "(new-object Net.WebClient).DownloadFile('https://bitnami.com/redirect/to/163844/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe', 'bitnami-wampstack-5.6.32-0-windows-x64-installer.exe')"
+bitnami-wampstack-5.6.32-0-windows-x64-installer.exe
 
 REM Open TCP Port 80 inbound and outbound
 netsh advfirewall firewall add rule name="Zoo TCP Port 80" dir=in action=allow protocol=TCP localport=80
@@ -12,13 +12,13 @@ netsh advfirewall firewall add rule name="Zoo TCP Port 80" dir=out action=allow 
 
 REM set file directory
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\archive
-xcopy Video_Archive_Web_Service\archive C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\archive /e /h /k
+xcopy archive C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\archive /e /h /k
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\image
-xcopy Video_Archive_Web_Service\image C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\image /e /h /k
+xcopy image C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\image /e /h /k
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources
-xcopy Video_Archive_Web_Service\resources C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources /e /h /k
-copy Video_Archive_Web_Service\index.html C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\index.html
-copy Video_Archive_Web_Service\php.ini C:\Bitnami\wampstack-5.6.32-0\php\php.ini
+xcopy resources C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources /e /h /k
+copy index.html C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\index.html
+copy php.ini C:\Bitnami\wampstack-5.6.32-0\php\php.ini
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\video
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\file
 
@@ -29,9 +29,9 @@ net stop wampstackMySQL
 net start wampstackMySQL
 mysql -uroot -pBig@1 -e "DROP DATABASE IF EXISTS archive_db"
 mysql -uroot -pBig@1 -e "CREATE DATABASE archive_db"
-mysql -uroot -pBig@1 archive_db < Video_Archive_Web_Service/archive_db.sql
+mysql -uroot -pBig@1 archive_db < archive_db.sql
 
 REM src remove
-rd /s /q Video_Archive_Web_Service
+REM rd /s /q Video_Archive_Web_Service
 
 pause
