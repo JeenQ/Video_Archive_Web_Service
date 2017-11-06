915e8c28f085f4f0d35016f82eea2049c6980adf
JeenQ <21200590@handong.edu>
2017-11-06 21:23:22 +0900

    window install modified 171106

--- install_win.bat
+++ install_win.bat
@@ -1,8 +1,9 @@
 @echo off
 
 REM install bitnami
+echo install start
 echo install bitnami
-REM powershell "(new-object Net.WebClient).DownloadFile('https://bitnami.com/redirect/to/163844/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe', 'install\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe')"
+powershell "(new-object Net.WebClient).DownloadFile('https://bitnami.com/redirect/to/163844/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe', 'install\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe')"
 install\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe
 
 REM set file directory
@@ -13,8 +14,8 @@ xcopy image C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\image /e /h /k
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources
 xcopy resources C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources /e /h /k
 copy index.html C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\index.html
-REM copy install\php.ini C:\Bitnami\wampstack-5.6.32-0\php\php.ini
-REM copy install\httpd.conf C:\Bitnami\wampstack-5.6.32-0\apache2\conf\httpd.conf
+copy install\php.ini C:\Bitnami\wampstack-5.6.32-0\php\php.ini
+copy install\httpd.conf C:\Bitnami\wampstack-5.6.32-0\apache2\conf\httpd.conf
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\video
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\file
 
@@ -22,10 +23,13 @@ REM DB initialization
 set path=%path%;C:\Bitnami\wampstack-5.6.32-0\mysql\bin;
 mysqladmin -uroot -p123456 password Big@1
 
+REM admin mode configuration
 "install\install_win_admin.bat_.lnk"
 
+REM create DB table
 mysql -uroot -pBig@1 -e "DROP DATABASE IF EXISTS archive_db"
 mysql -uroot -pBig@1 -e "CREATE DATABASE archive_db"
 mysql -uroot -pBig@1 archive_db < install\archive_db.sql
 
+echo install finish
 pause
