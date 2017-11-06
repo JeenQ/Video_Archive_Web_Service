a4ab0572ce1bf510fa48ad18c8a18513d9124b02
JeenQ <21200590@handong.edu>
2017-11-06 21:20:59 +0900

    window install, index.html modified 171106

--- install/install_win_admin.bat
+++ install/install_win_admin.bat
@@ -6,6 +6,7 @@ netsh advfirewall firewall add rule name="Zoo TCP Port 80" dir=out action=allow 
 
 REM DB initialization
 set path=%path%;C:\Bitnami\wampstack-5.6.32-0\mysql\bin;
-mysqladmin -uroot -p123456 password Big@1
 net stop wampstackMySQL
 net start wampstackMySQL
+net stop wampstackApache
+net start wampstackApache
--- install_win.bat
+++ install_win.bat
@@ -2,7 +2,7 @@
 
 REM install bitnami
 echo install bitnami
-powershell "(new-object Net.WebClient).DownloadFile('https://bitnami.com/redirect/to/163844/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe', 'install\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe')"
+REM powershell "(new-object Net.WebClient).DownloadFile('https://bitnami.com/redirect/to/163844/bitnami-wampstack-5.6.32-0-windows-x64-installer.exe', 'install\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe')"
 install\bitnami-wampstack-5.6.32-0-windows-x64-installer.exe
 
 REM set file directory
@@ -13,13 +13,14 @@ xcopy image C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\image /e /h /k
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources
 xcopy resources C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\resources /e /h /k
 copy index.html C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\index.html
-copy install\php.ini C:\Bitnami\wampstack-5.6.32-0\php\php.ini
-copy install\httpd.conf C:\Bitnami\wampstack-5.6.32-0\apache2\conf\httpd.conf
+REM copy install\php.ini C:\Bitnami\wampstack-5.6.32-0\php\php.ini
+REM copy install\httpd.conf C:\Bitnami\wampstack-5.6.32-0\apache2\conf\httpd.conf
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\video
 mkdir C:\Bitnami\wampstack-5.6.32-0\apache2\htdocs\file
 
 REM DB initialization
 set path=%path%;C:\Bitnami\wampstack-5.6.32-0\mysql\bin;
+mysqladmin -uroot -p123456 password Big@1
 
 "install\install_win_admin.bat_.lnk"
 
